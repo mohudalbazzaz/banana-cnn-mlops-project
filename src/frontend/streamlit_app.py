a93f5537dbeeffee4c4b03650e621e6a5e7d45e4
Mohud Al-Bazzaz <mohudalbazzaz@yahoo.co.uk>
2026-04-20 22:33:56 +0100

    more testing

--- src/frontend/streamlit_app.py
+++ src/frontend/streamlit_app.py
@@ -5,8 +5,8 @@ import os
 
 load_dotenv()
 
-BACKEND_URL = os.getenv("BACKEND_URL", "http://backend:8000")
-
+#BACKEND_URL = os.getenv("BACKEND_URL", "http://backend:8000")
+BACKEND_URL = "http://backend:8000"
 
 def run_ui() -> None:
     """
